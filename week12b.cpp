e9226a3c6188db6fb163807143bdeeb2b6f73f8a
rishmithak <44499269+rishmithak@users.noreply.github.com>
2019-04-25 08:33:38 +0530

    Update week12b.cpp

--- week12b.cpp
+++ week12b.cpp
@@ -1,3 +1,13 @@
+//<start>
+/*
+12. b. To write a program to read a set of lines from
+the keyboard and store it on a specified file
+
+Name: Koppuravuri Sai Naga Rishmitha
+Section: IT-A
+Roll no: 18121A1245
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,4 +26,15 @@ int main () {
     cout<<"Enter text and terminate with $:\n";
     std::getline(std::cin, str, delim);
     file<<str;
+    cout<<"/n/nText write in file completed";
 }
+
+/*
+Output:
+I love studying human evolution
+I can't solve the Rubik's
+I love quantum physics$
+
+Text write in file completed
+*/
+//<Stop>
