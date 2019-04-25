d4754c7d40979357cb3a645e879d364d254064b1
rishmithak <44499269+rishmithak@users.noreply.github.com>
2019-04-25 08:27:10 +0530

    Update week12a.cpp

--- week12a.cpp
+++ week12a.cpp
@@ -1,3 +1,12 @@
+//</start>
+/* 12. a. To write a program to write numbers
+1 to 100 in a data file NOTES.TXT
+
+Name: Koppuravuri Sai Naga Rishmitha
+Section: IT-A
+Roll no: 18121A1245
+*/
+
 #include<iostream>
 #include<fstream>
 using namespace std;
@@ -10,4 +19,11 @@ int main()
     {
         file<<i<<endl;
     }
+    cout<<"File is created";
 }
+
+/*
+Output:
+File is created
+*/
+//<Stop>
